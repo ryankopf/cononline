1f0000cc1435f5828d56cbfee39b3ab1dec695a6
ryankopf <ryan@ryankopf.com>
2020-04-07 21:09:24 -0500

    Adding images and storage.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :products
   resources :vendors
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'pages:index'
 end
