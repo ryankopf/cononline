74a7bc146d5b6f5ffd28d575291e27ff7dd37f21
ryankopf <ryan@ryankopf.com>
2020-04-16 23:40:59 -0500

    Working on carts and ordering.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,6 +8,12 @@ class Cart < ApplicationRecord
     cart_product.increment!(:quantity)
   end
 
+  def remove_product(product,quantity)
+    cart_product = Cartproduct.find_or_create_by(cart_id: self.id, product_id: product.id)
+    cart_product.decrement!(:quantity)
+    cart_product.destroy if quantity <= 0
+  end
+
   def paid!(params)
     amount_paid = (params[:mc_gross].to_f*100).to_i
     paypal_fee = (0 - (params[:mc_fee].to_f*100).to_i)
@@ -17,8 +23,7 @@ class Cart < ApplicationRecord
     quantities = params[:quantities]
     product_ids = params[:products]
     product_ids.each_with_index do |product_id, i|
-      products = self.cartproducts.find_by(product_id: product_id).decrement!(quantities[i])
-      products.destroy if products.quantity <= 0
+      remove_product(product_id, quantities[i])
     end
     order = Order.create(user_id: self.user_id, vendor_id: params[:vendor_id])
     order.add_items(products, quantities)
