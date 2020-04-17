1c2dbcc965222e41e849405720f8832149cac381
ryankopf <ryan@ryankopf.com>
2020-04-17 00:14:09 -0500

    Working on carts and ordering.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,11 @@
 class Order < ApplicationRecord
+  belongs_to :user
   has_many :orderproducts
   has_many :payments
 
   def add_items(product_ids,quantities)
     product_ids.each_with_index do |product_id, i|
-      Orderproduct.create(order: self, product_id: product_id, quantity: quantities[i].to_i)
+      Orderproduct.create(order: self, product_id: product_id, quantity: quantities[i].to_i, price: Product.find(product_id))
     end
   end
   def add_payment
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -5,11 +5,18 @@ class CartTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "create a cart and process an order" do
-    assert_difference('Order.count') do
+    cart = nil
+    assert_difference('Cart.count') do
       cart = Cart.create(user_id: User.first.id)
+    end
+    assert_difference('Cartproduct.count', 1) do
       cart.cartproducts.create(product_id: Product.first.id, quantity: 1)
-      cart.paid!({mc_gross: 100, mc_fee: 100, txn_id: 'test', payer_email: 'some@body.com', receiver_email: 'some@bodyelse.com', quantities: '1', products: "#{Product.first.id}",
+    end
+    assert_difference('Order.count',1) do
+      assert_difference('Orderproduct.count',1) do
+        cart.paid!({mc_gross: 100, mc_fee: 100, txn_id: 'test', payer_email: 'some@body.com', receiver_email: 'some@bodyelse.com', quantities: '1', products: "#{Product.first.id}",
                   vendor_id: Product.first.vendor_id})
+      end
     end
   end
 end
