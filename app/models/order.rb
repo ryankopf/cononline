4fca33e643d8e9da23f4273cdaa461dc26f5a61a
ryankopf <ryan@ryankopf.com>
2020-04-16 23:30:55 -0500

    Working on carts and ordering.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,9 @@
 class Order < ApplicationRecord
+  has_many :orderproducts
 
   def add_items(products,quantities)
     products.each_with_index do |product_id, i|
-      Orderitem.create(order: self, product_id: product_id, quantity: quantities[i])
+      Orderproduct.create(order: self, product_id: product_id, quantity: quantities[i])
     end
   end
   def add_payment
